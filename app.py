09f8385910b0bd70857f822e735e06ebd1d207df
Joseph Lee <z.joseph.lee.z@gmail.com>
2022-04-26 23:53:01 -0400

    stuff

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, send_from_directory, jsonify
+
+from datetime import datetime
+from os.path import exists
+
+TIMES_FILE = "times.txt"
+
+if not exists(TIMES_FILE):
+    open(TIMES_FILE, 'w').close()
+
+DATETIME_FORMAT = "%Y-%m-%d %I:%M %p"
+JS_LOCALE_FORMAT = "%-m/%-d/%Y %-I:%M:%S %p"
+
+
+times = []
+with open(TIMES_FILE, 'r') as f:
+    for line in f:
+        times.append(datetime.strptime(line.strip(), DATETIME_FORMAT))
+
+def append_time(t):
+    with open(TIMES_FILE, 'a') as f:
+        f.write(datetime.strftime(t, DATETIME_FORMAT) + "\n")
+
+##################
+## Flask stuff
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def index():
+    return render_template('index.html', times=[datetime.strftime(t, JS_LOCALE_FORMAT) for t in times])
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/buttonclicked', methods=['POST'])
+def button_clicked():
+    t = datetime.now()
+    append_time(t)
+    times.append(t)
+    return {"times": ",".join([t.isoformat() for t in times])}
+
+@app.route('/resettimes', methods=['POST'])
+def reset_times():
+    global times
+    print("got request to reset")
+    print(times)
+    open(TIMES_FILE, 'w').close()
+    times = []
+    return {"times": ",".join([t.isoformat() for t in times])}
+
+#### MAIN
+
+if __name__ == '__main__':
+    socket_.run(app, debug=True)
---
+++ js/main.js
@@ -0,0 +1,109 @@
+console.log("start");
+
+const add = (x,y) =>
+  x + y
+  
+const sum = xs =>
+  xs.reduce(add, 0)
+
+const average = xs =>
+  xs[0] === undefined ? NaN : sum(xs) / xs.length
+
+const delta = ([x,...xs]) =>
+  xs.reduce(([acc, last], x) => [[...acc, x-last], x], [[], x]) [0]
+
+const roundTo1Decimal = x => Math.round(x * 10) / 10
+
+
+function updateTimes(all_times) {
+  // number of times, 5am-5am
+  var threshold = new Date(Math.floor(((new Date()).getTime() - 1000 * 60 * 60 * 9) / 86400000) * 86400000 + 1000 * 60 * 60 * 9);    
+  var num = 0;
+  for (let time of all_times) {
+    if (time > threshold) {
+      num += 1;
+    }
+  }
+  document.getElementById('stat1').textContent = num;
+
+  // average time between times, past 24 
+  var threshold = new Date((new Date()).getTime() - 1000 * 60 * 60 * 24);
+  var new_times = [];
+  for (let time of all_times) {
+    if (time > threshold) {
+      new_times.push(time.getTime());
+    }
+  }
+  var avg = average(delta(new_times)) / (1000 * 60);
+  document.getElementById('stat2').textContent = "" + roundTo1Decimal(avg) + " min";
+
+  // average number of times per day in past 7 days
+  var counts = {};
+  var threshold1 = new Date(Math.floor(((new Date()).getTime() - 1000 * 60 * 60 * 9) / 86400000) * 86400000 + 1000 * 60 * 60 * 9);
+  var threshold2 = new Date(Math.floor(((new Date()).getTime() - 1000 * 60 * 60 * 9) / 86400000) * 86400000 + 1000 * 60 * 60 * 9 + 1000 * 60 * 60 * 24);
+
+  for (var i = 0; i < 7; i++) {
+    counts[i] = 0;
+    for (let time of all_times) {
+      if (time > threshold1 && threshold2 > time) {
+        counts[i] += 1;
+      }
+    }
+    threshold1 = new Date(threshold1.getTime() - 1000 * 60 * 60 * 24);
+    threshold2 = new Date(threshold2.getTime() - 1000 * 60 * 60 * 24);
+  }
+
+  document.getElementById('stat3').textContent = roundTo1Decimal(average(Object.values(counts)));
+
+  // average time between times, past 7 days
+  var threshold = new Date((new Date()).getTime() - 1000 * 60 * 60 * 24 * 7);
+  var new_times = [];
+  for (let time of all_times) {
+    if (time > threshold) {
+      new_times.push(time.getTime());
+    }
+  }
+  var avg = average(delta(new_times)) / (1000 * 60);
+  document.getElementById('stat4').textContent = "" + roundTo1Decimal(avg) + " min";
+  
+  
+  var times = document.getElementById('times');
+  while (times.firstChild) {
+    times.removeChild(times.firstChild);
+  }
+
+  for (let time of all_times) {
+    var item = document.createElement('div');
+    item.textContent = time.toLocaleString();
+    times.appendChild(item);
+  }
+}
+
+function resetTimes() {
+  if (confirm("Are you sure you want to reset?") == true) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/resettimes", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({}));
+    updateTimes([]);
+  }
+}
+
+function buttonClicked() {
+  console.log("oijasoijef");
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/buttonclicked", true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({}));
+
+  xhr.onload = function() {
+    var data = JSON.parse(this.responseText);
+    console.log("got response");
+    console.log(data);
+    var all_times = data.times.split(",").map(x => new Date(x));
+    updateTimes(all_times);
+    
+  };
+  console.log("after post request");
+}
